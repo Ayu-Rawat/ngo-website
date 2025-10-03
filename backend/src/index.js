daaf69f3b32ebc2831028470879ff35fa747b2c2
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2025-10-03 13:39:52 +0530

    setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import {app} from './app.js'
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);

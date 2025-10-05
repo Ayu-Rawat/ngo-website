2d990327591b50f0ecbdbf8666ac59871a594288
AnmolPlayzz <anmolisnotavailable@gmail.com>
2025-10-06 02:34:38 +0530

    feat: Update About page content and styles; add container for improved layout

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -3,7 +3,22 @@ import styles from './page.module.css'
 export default function About() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>About</h1>
+      <div className={styles.container}>
+        <h1>About Us</h1>
+        Change I Network (CIN) is an NGO with a mission to bring positive change in the lives of people.
+
+        Change I Network started its operations about two years back, with its headquarters in New Delhi. CIN's members are running four of its flagship programmes (4 Ps) through its branches in different parts of the country.
+
+        Genesis
+        “We can’t help everyone, but everyone can help someone.” Inspired by this very quote, a group of professionals came together to form Change I Network. Thus, it is being run by working professionals who are committed to dedicating their time, resources, and skills to serve society.
+
+        This organization strives to bring about a positive change in society by helping people.
+
+        The concept of CIN originated from the anti-corruption and socio-political movements happening across the globe, which have seen immense youth participation.
+
+        Vision and Mission
+        To enable civil society across the world to engage proactively in the change process through the philosophy of civic-driven change and to bring sustainable change in the lives of underprivileged children, youth, women, and the elderly.
+      </div>
     </main>
   );
 }
